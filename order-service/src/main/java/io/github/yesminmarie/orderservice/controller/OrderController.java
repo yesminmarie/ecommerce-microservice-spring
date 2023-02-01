aa3134297a50a868543c05c519c6aeacab526c38
Yesmin Lahoud <ymslahoud@gmail.com>
2023-02-01 11:55:19 -0300

    circuit breaker

--- order-service/src/main/java/io/github/yesminmarie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/io/github/yesminmarie/orderservice/controller/OrderController.java
@@ -1,10 +1,15 @@
 package io.github.yesminmarie.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import io.github.yesminmarie.orderservice.dto.OrderRequest;
 import io.github.yesminmarie.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -15,8 +20,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfuly";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()
+                -> "Oops! Something went wrong, please order after some time");
     }
 }
